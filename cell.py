d93d02f8747680d6b3f132434045493ee236ed79
gdx <gdx>
2021-08-21 22:40:40 +0100

    Grid; More buttons;

---
+++ cell.py
@@ -0,0 +1,145 @@
+from tkinter import *
+
+class Cell:
+    def __init__(self,parent,x,y,dummyImage,state = 0,cellSize = 50):
+        #btn = ttk.Button(gridFrame,text=x*10+y,image=gridCellImage,style="GStyle.TButton",compound=CENTER)
+        #self.button = Button(parent,text=x*10+y,image=dummyImage,width=50,height=50,compound=CENTER,background="#cccccc",relief=SUNKEN)
+        self.button = Label(parent,text=x*10+y,image=dummyImage,width=cellSize,height=cellSize,compound=CENTER,background="#cccccc",relief=FLAT)
+        self.button.grid(row=x,column=y,padx=1,pady=1)
+        self.state = state
+        if state != 0:
+            self.update()
+        #btn.configure(command=onCellClick(gridState[x][y]))
+        #self.button.bind("<Button-1>",lambda f: onCellClick(gridState[x][y],True))
+        #self.button.bind("<Button-3>",lambda f: onCellClick(gridState[x][y]))
+
+    # blocks or unblocks the cell depending on its current state
+    def block(self):
+        if self.state == 3:
+            self.state = 0
+        else:
+            self.state = 3
+        self.update()
+
+    def clear(self):
+        self.state = 0
+        self.update()
+
+    # updates the button graphic for the cell's state
+    def update(self):
+        if (self.state == 0): # none
+            self.button.configure(relief=FLAT,background="#cccccc")
+        elif (self.state == 1): # start
+            self.button.configure(relief=RAISED,background="#778beb")
+        elif (self.state == 2): # end
+            self.button.configure(relief=RAISED,background="#cf6a87")
+        elif (self.state == 3): # blocked
+            self.button.configure(relief=FLAT,background="#666666")
+        elif (self.state == 4): # path
+            self.button.configure(relief=FLAT,background="#f19066")
+
+class Grid:
+    def __init__(self,parent,dummyImage,onCellClick,grid=None,cellSize=30,x=0,y=0):
+        if grid is None:
+            self.dimensions = (x,y)
+            self.state = {k:dict() for k in range(y)} # [row][col]: (button,state) - states: 0 - none; 1 - start; 2 - end; 3 - blocked
+        for row in range(y): # rows
+            for col in range(x): # columns
+                #btn = ttk.Button(gridFrame,text=x*10+y,image=gridCellImage,style="GStyle.TButton",compound=CENTER)
+                #btn = Button(gridFrame,text=x*10+y,image=dummyImage,width=50,height=50,compound=CENTER,background="#cccccc",relief=SUNKEN)
+                #btn.grid(row=x,column=y)
+                if self.state[row].get(col) == 2:
+                    self.state[row][col] = Cell(parent,row,col,dummyImage,1,cellSize)
+                    self.start = self.state[row][col]
+                elif self.state[row].get(col) == 3:
+                    self.state[row][col] = Cell(parent,row,col,dummyImage,2,cellSize)
+                    self.end = self.state[row][col]
+                else:
+                    self.state[row][col] = Cell(parent,row,col,dummyImage,cellSize=30)
+                #btn.configure(command=onCellClick(gridState[x][y]))
+                self.state[row][col].button.bind("<Button-1>",lambda f,cell=self.state[row][col]: onCellClick(cell,True))
+                self.state[row][col].button.bind("<Button-3>",lambda f,cell=self.state[row][col]: onCellClick(cell))
+        if grid is None: # set start/end nodes
+            self.state[0][0].state = 1
+            self.state[0][0].update()
+            self.start = self.state[0][0]
+            self.state[y-1][x-1].state = 2
+            self.state[y-1][x-1].update()
+            self.end = self.state[y-1][x-1]
+
+    def replaceStart(self,cell):
+        if cell is self.start:
+            self.start = self.end
+            self.end = cell
+            self.start.state = 1
+            self.end.state = 2
+            self.start.update()
+            self.end.update()
+        elif cell is self.end:
+            self.end = self.start
+            self.start = cell
+            self.start.state = 1
+            self.end.state = 2
+            self.start.update()
+            self.end.update()
+        else:
+            self.start.state = 0
+            self.start.update()
+            self.start = cell
+            cell.state = 1
+            self.start.update()
+
+    def replaceEnd(self,cell):
+        if cell is self.end:
+            self.end = self.start
+            self.start = cell
+            self.start.state = 1
+            self.end.state = 2
+            self.start.update()
+            self.end.update()
+        elif cell is self.start:
+            self.start = self.end
+            self.end = cell
+            self.start.state = 1
+            self.end.state = 2
+            self.end.update()
+            self.start.update()
+        else:
+            self.end.state = 0
+            self.end.update()
+            self.end = cell
+            cell.state = 2
+            self.end.update()
+
+    # grid start/end integrity check
+    def integrityCheck(self):
+        """start = False
+        end = False
+        for vr in self.state.values():
+            for vc in vr.values():
+                if vc.state == 1:
+                    if start:
+                        vc.state = 0
+                        vc.update()
+                    else:
+                        start = True
+                elif vc.state == 2:
+                    if end:
+                        vc.state = 0
+                        vc.update()
+                    else:
+                        end = True"""
+        if not self.start:
+            if self.state[0][0].state == 2:
+                self.state[self.dimensions[0]-1][self.dimensions[1]-1].state = 1
+                self.state[self.dimensions[0]-1][self.dimensions[1]-1].update()
+            else:
+                self.state[0][0].state = 1
+                self.state[0][0].update()
+        if not self.end:
+            if self.state[self.dimensions[0]-1][self.dimensions[1]-1].state == 1:
+                self.state[0][0].state = 2
+                self.state[0][0].update()
+            else:
+                self.state[self.dimensions[0]-1][self.dimensions[1]-1].state = 2
+                self.state[self.dimensions[0]-1][self.dimensions[1]-1].update()
--- main.py
+++ main.py
@@ -1,36 +1,52 @@
 from tkinter import *
 import tkinter.ttk as ttk
+from cell import *
 
 # create the main window
 window = Tk()
 window.title("PathFindingPy")
 #window.geometry("500x300")
 
+# create positional frames
+leftFrame = Frame(window)
+leftFrame.pack(side=LEFT,fill=BOTH)
+rightFrame = Frame(window)
+rightFrame.pack(side=RIGHT,fill=BOTH)
+
+# transparent image for cell sizing
+cellSize = 40
+dummyImage = PhotoImage(width=1,height=1)
+
 # styles
-radioStyle = ttk.Style()
-radioStyle.theme_use("default")
-radioStyle.configure("NStyle.TRadiobutton",
+nStyle = ttk.Style()
+nStyle.theme_use("clam")
+nStyle.configure("NStyle.TRadiobutton",
     indicatorrelief=FLAT,
-    indicatormargin=-1,
+    indicatormargin=-10, # -1 for default
     indicatordiameter=-1,
     relief=RAISED,
     focusthickness=0, highlightthickness=0, padding=15, background="#aea4de")
-radioStyle.map('NStyle.TRadiobutton',
+nStyle.map('NStyle.TRadiobutton',
     background=[('selected', '#574b90'), ('active', '#786fa6')])
-radioStyle.configure("NStyle.TButton",
+nStyle.configure("NStyle.TButton",
     padding=10, background="#aea4de")
-radioStyle.map('NStyle.TButton',
+nStyle.map('NStyle.TButton',
     background=[('selected', '#574b90'), ('active', '#786fa6')])
+nStyle.configure("StopButton.TButton",
+    padding=10, background="#e66767")
+nStyle.map('StopButton.TButton',
+    background=[('selected', '#ea8685'), ('active', '#ea8685')])
+nStyle.configure("Horizontal.NStyle.TScale")
 
 # event handlers
 def onAlgorithmChanged():
     print("Alg: ", algorithm.get())
-    window.focus() # to remove focus from the button
+    window.focus() # remove focus from the button
 
 # algorithm selection
 algorithm = IntVar()
 algorithmFunc = None # todo
-algFrame = LabelFrame(window,text="Algorithms",padx=10,pady=10,width=200)
+algFrame = LabelFrame(leftFrame,text="Algorithms",padx=10,pady=10,width=200)
 algFrame.pack(anchor=NW,padx=5)
 ttk.Radiobutton(algFrame,text="DepthFirstSearch",variable=algorithm, value=1,style="NStyle.TRadiobutton", command=onAlgorithmChanged).pack(fill=X,pady=[0,10])
 ttk.Radiobutton(algFrame,text="BreadthFirstSearch",variable=algorithm, value=2, style="NStyle.TRadiobutton", command=onAlgorithmChanged).pack(fill=X,pady=[0,10])
@@ -38,12 +54,85 @@ ttk.Radiobutton(algFrame,text="Dijkstra",variable=algorithm, value=3, style="NSt
 ttk.Radiobutton(algFrame,text="A*",variable=algorithm, value=4, style="NStyle.TRadiobutton", command=onAlgorithmChanged).pack(fill=X,pady=[0,0])
 
 # execution state
-state = 0 # 0 - idle, 1 - running, 2 - stopped, 3 - finished
-# TODO - understand width=20 ????
-ttk.Button(window,text="Run",style="NStyle.TButton",command=None,width=20).pack(anchor=NW,padx=[8,10],pady=[10,0])
-ttk.Button(window,text="Clear",style="NStyle.TButton",command=None,width=20).pack(anchor=NW,padx=[8,10],pady=[5,10])
+state = 0 # 0 - idle, 1 - running, 2 - stopped, 3 - step, 4 - finished
+startEndFlip = False # used to determine whether placing a Start or End node (flips on click)
+speed = DoubleVar()
+
+runPauseButton = ttk.Button(leftFrame,text="Run",style="NStyle.TButton",width=20)
+runPauseButton.pack(anchor=NW,padx=[8,10],pady=[10,0])
+stepButton = ttk.Button(leftFrame,text="Step",style="NStyle.TButton",width=20)
+stepButton.pack(anchor=NW,padx=[8,10],pady=[5,0])
+stopButton = ttk.Button(leftFrame,text="Stop",style="StopButton.TButton",width=20)
+stopButton.pack(anchor=NW,padx=[8,10],pady=[5,0])
+speedFrame = LabelFrame(leftFrame,text="Speed",padx=22,pady=0)
+speedFrame.pack(anchor=NW,padx=5)
+speedScale = Scale(speedFrame,variable=speed,orient=HORIZONTAL,resolution=0.5,from_=0.5,to=2,tickinterval=0.5)
+#speedScale = ttk.Scale(speedFrame,variable=speed,orient=HORIZONTAL,from_=0.5,to=2,style="Horizontal.NStyle.TScale")
+speedScale.pack(fill=X,padx=0)
+speedScale.set(1)
+clearButton = ttk.Button(leftFrame,text="Clear",style="StopButton.TButton",width=20)
+clearButton.pack(anchor=NW,padx=[8,10],pady=[5,5])
+
+# event handlers
+def onRunStopClicked():
+    global state
+    if state == 0:
+        state = 1
+        runPauseButton.configure(text="Pause")
+    elif state == 1:
+        state = 2
+        runPauseButton.configure(text="Resume")
+    elif state == 2:
+        state = 1
+        runPauseButton.configure(text="Pause")
+    elif state == 3:
+        state = 1
+        runPauseButton.configure(text="Pause")
+    window.focus() # remove focus from the button
+
+# set handlers
+runPauseButton.configure(command=onRunStopClicked)
 
 # grid - this consists of a NxN button grid
+gridSize = (8,8) # width,height
+#gridState = {k:dict() for k in range(gridSize[1])} # [row][col]: (button,state) - states: 0 - none; 1 - start; 2 - end; 3 - blocked
+gridFrame = Frame(rightFrame,padx=1,pady=1,background="#666666")
+gridFrame.pack(anchor=NE,padx=5, pady=5)
+
+# stats display
+stateLabel = Label(rightFrame,text="State: idle")
+stateLabel.pack(anchor=NW,padx=5)
+iterationsLabel = Label(rightFrame,text="Iterations: ")
+iterationsLabel.pack(anchor=NW,padx=5)
+
+# click handler
+def onCellClick(cell,left=False):
+    if state != 0:
+        return
+    if left: # block/unblock
+        cell.block() # calls update
+    else: # start/end
+        global startEndFlip
+        startEndFlip = not startEndFlip
+        if startEndFlip:
+            grid.replaceStart(cell)
+        else:
+            grid.replaceEnd(cell)
+    # check start/end integrity
+    #grid.integrityCheck()
+
+# generate grid
+'''for x in range(gridSize[1]): # rows
+    for y in range(gridSize[0]): # columns
+        #btn = ttk.Button(gridFrame,text=x*10+y,image=gridCellImage,style="GStyle.TButton",compound=CENTER)
+        #btn = Button(gridFrame,text=x*10+y,image=dummyImage,width=50,height=50,compound=CENTER,background="#cccccc",relief=SUNKEN)
+        #btn.grid(row=x,column=y)
+        gridState[x][y] = Cell(gridFrame,x,y,dummyImage,30)#(btn,0)
+        #btn.configure(command=onCellClick(gridState[x][y]))
+        gridState[x][y].button.bind("<Button-1>",lambda f,cell=gridState[x][y]: onCellClick(cell,True))
+        gridState[x][y].button.bind("<Button-3>",lambda f,cell=gridState[x][y]: onCellClick(cell))'''
+
+grid = Grid(gridFrame,dummyImage,onCellClick,cellSize=cellSize,x=gridSize[0],y=gridSize[1])
 
 # run loop
 window.mainloop()
